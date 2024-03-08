dda1c0cbebb77f4f7e2a23ea274fe217b7ee91d6
loweffort-alt <farfan_alexander@outlook.com>
2024-03-08 04:18:07 -0500

    Initial commit: Test server original

---
+++ server.js
@@ -0,0 +1,42 @@
+import express from "express";
+import cors from "cors";
+import "dotenv/config";
+const app = express();
+
+app.use(cors());
+
+app.use("/prueba.json", async (req, res) => {
+  const fakeData = [
+    {
+      id: 239,
+      NumeroReporte: 116,
+      FechaLocal: "2024-02-19",
+      HoraLocal: "11:23:13",
+      Magnitud: 5.5,
+      Referencia: "AHORA SI CAMBIA",
+      Latitud: -9.44,
+      Longitud: -79.14,
+      Profundidad: 35,
+      Intensidad: "IV-V Chimbote",
+      simulacro: 0,
+      created_at: "2024-02-19T11:26:53.000-05:00",
+      sound: false,
+      TipoMagnitud: "",
+      Mensaje: "",
+      ReferenciaUnidad: "",
+      Simulacro: "",
+      Mapa: "",
+      FechaLocalEnviaSSN: "",
+      FechaRecepcion: "",
+      UsuarioRegistra: "",
+      createdAt: "",
+      updatedAt: "",
+    },
+  ];
+  res.json(fakeData);
+});
+
+// Inicia el servidor
+app.listen(process.env.PORT, () => {
+  console.log(`Servidor proxy escuchando en el puerto ${process.env.PORT}`);
+});
